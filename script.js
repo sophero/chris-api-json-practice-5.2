418a56e0c4959991a7b87550160b65c25e6f65ec
Sophia Di <backwardsxmarathon@gmail.com>
2017-05-02 16:30:46 -0400

    Adds date to gallery items, introduces use of newDiv function within generatePosts, adds display flex layout to gallery.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-
 var gallery = new PostGallery();
+gallery.getJSON();
 
 function PostGallery() {
 	this.getJSON = getJSON;
@@ -20,6 +20,10 @@ function PostGallery() {
 			success: function(response) {
 				console.log(response.posts);
 				generatePosts(response.posts);
+			},
+			error: function(errorObj) {
+				// console.log(errorObj);
+				console.log("getting an XMLHttpRequest error message? Make sure Cross-Origin Resource Sharing is enabled. You may need to reactivate it.");
 			}
 
 		});		
@@ -28,27 +32,35 @@ function PostGallery() {
 
 	function generatePosts(posts) {
 
-		// using forEach:
 		posts.forEach(function(post) {
-			var newDiv = "<div class='gallery-item'>"
-			var imageDivStr = "<div class='gallery-item__img'></div>";
-			newDiv += imageDivStr;
-			newDiv += post.title;
-			newDiv += "</div>";
-			
-			postGallery.innerHTML += newDiv;
+			var dateDiv = newDiv("gallery-item__date", parseDate(post.date));
+			var imageDiv = newDiv("gallery-item__img", "");
+			var titleDiv = newDiv('gallery-item__title', post.title);
+
+			var galleryItemDiv = newDiv('gallery-item', imageDiv + dateDiv + titleDiv); 
+			postGallery.innerHTML += galleryItemDiv;
 
 		});
 
 
 		[].forEach.call(posts, function(post, index) {
 			var imageDiv = document.getElementsByClassName('gallery-item__img')[index];
-			console.log(imageDiv);
 			imageDiv.style.backgroundImage = "url('" + post.img + "')";
-
 		});
 
+		function newDiv(classStr, divContent) {
+			var newDiv = "<div class='" + classStr + "'>";
+			newDiv += divContent + "</div>";
+			return newDiv;
+		}
 
+		function parseDate(dateStr) {
+			var monAbbrArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+			var split = dateStr.split("/");
+			var mon = monAbbrArray[split[0]]; //American Date!
+			var day = split[1];
+			return mon + day;
+		}
 
 
 		// var images = document.getElementsByClassName('gallery-item__img');
